bda7485cd27882f1d238c68530ab4b8c40a2c2e8
mullihpeter <muli.peter121@gmail.com>
2023-01-16 16:03:00 +0100

    html scripting

--- feed/models.py
+++ feed/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 # Create your models here.
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE())
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     text = models.TextField(max_length=400)
     created_date = models.DateField(default=timezone.now)
